4aaaea502e5f1d685c7742ebce149380d3f31a15
Sheridan Quispe <nelida.quispetacuri@gmail.com>
2020-09-29 23:45:51 -0500

    feat(app): Agrega RouterModule y configura rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { RouterModule } from '@angular/router'; ==> Importar Angular Router
 import { HttpClientModule } from '@angular/common/http';
 
 // Imports for loading & configuring the in-memory web api
@@ -9,15 +10,22 @@ import { ProductData } from './products/product-data';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-
 /* Feature Modules */
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [
-    BrowserModule,
+  imports: [ // Matriz de importaciones del decorador
+    BrowserModule, // Por default
+    RouterModule.forRoot(ROUTES, { useHash: true, enableTracing: true}),  // ==> Importar Angular Router
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
@@ -29,6 +37,6 @@ import { MessageModule } from './messages/message.module';
     WelcomeComponent,
     PageNotFoundComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Por default
 })
 export class AppModule { }
